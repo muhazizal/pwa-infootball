7d7d1297f73c8e905e652beb6329b52aedf8d914
muhazizal <muhazizal.26@gmail.com>
2020-06-30 19:51:24 +0700

    added and setup webpack-pwa-manifest, sw-precahce-webpack-plugin

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
-// Import Materialize SCSS & JS
+// Import materialize scss and js
 import './js/materialize.min';
 import './sass/materialize.scss';
 
-// Import Views
+// Import views
 import './js/components/navbar.js';
+
+// Register service worker
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker
+			.register('./service-worker.js')
+			.then(function () {
+				console.log('Pendaftaran ServiceWorker berhasil');
+			})
+			.catch(function () {
+				console.log('Pendaftaran ServiceWorker gagal');
+			});
+	});
+} else {
+	console.log('ServiceWorker belum didukung browser ini.');
+}
--- webpack.common.js
+++ webpack.common.js
@@ -1,16 +1,20 @@
 // Init
 const path = require('path');
+const isDevelopment = process.env.NODE_ENV === 'development';
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const isDevelopment = process.env.NODE_ENV === 'development';
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const PUBLIC_PATH = 'http://localhost:8080/';
 
 // Exports
 module.exports = {
 	entry: './src/app.js',
 	output: {
-		filename: 'bundle.js',
+		filename: isDevelopment ? 'bundle.js' : 'bundle.[contenthash].js',
 		path: path.resolve(__dirname, 'dist'),
+		publicPath: PUBLIC_PATH,
 	},
 	module: {
 		rules: [
@@ -51,7 +55,7 @@ module.exports = {
 				loader: 'file-loader',
 				options: {
 					name: '[path][name].[ext]',
-					outputPath: 'images',
+					outputPath: path.join('assets', 'images'),
 				},
 			},
 			// Fonts Loader
@@ -61,7 +65,8 @@ module.exports = {
 					{
 						loader: 'file-loader',
 						options: {
-							outputPath: 'fonts',
+							name: '[path][name].[ext]',
+							outputPath: path.join('assets', 'fonts'),
 						},
 					},
 				],
@@ -93,6 +98,33 @@ module.exports = {
 			template: './src/index.html',
 			filename: 'index.html',
 			favicon: './src/assets/icons/infootball-512.png',
+		}),
+		new SWPrecacheWebpackPlugin({
+			cacheId: 'pwa-infootball-v1',
+			filename: 'service-worker.js',
+			staticFileGlobs: ['./src/index.html', './src/app.js', './src/assets/icons/**.*'],
+			mergeStaticsConfig: true,
+			minify: true,
+			navigateFallback: PUBLIC_PATH + 'index.html',
+			staticFileGlobsIgnorePatterns: [/\.map$/],
+		}),
+		new WebpackPwaManifest({
+			name: 'PWA Infootball',
+			short_name: 'Infootball',
+			description: 'PWA about football information',
+			background_color: '#f1f1f1',
+			theme_color: '#f1f1f1',
+			'theme-color': '#f1f1f1',
+			ios: true,
+			crossorigin: 'use-credentials',
+			start_url: '/index.html',
+			icons: [
+				{
+					src: path.resolve('./src/assets/icons/infootball-512.png'),
+					sizes: [96, 128, 192, 256, 384, 512],
+					destination: path.join('assets', 'icons'),
+				},
+			],
 		}),
 	],
 };
