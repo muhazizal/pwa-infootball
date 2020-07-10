6add0a029d31175c11288acc974068d6a6da9e8f
muhazizal <muhazizal.26@gmail.com>
2020-07-10 14:50:16 +0700

    added push notification event on sw

--- src/js/reg-sw.js
+++ src/js/reg-sw.js
@@ -1,8 +1,28 @@
 import runtime from 'serviceworker-webpack-plugin/lib/runtime';
 
-// Register service worker
+// Request push notification permission
+const requestPermission = () => {
+	if ('Notification' in window) {
+		Notification.requestPermission().then(result => {
+			if (result === 'denied') {
+				console.log('Notification permission denied');
+				return;
+			} else if (result === 'default') {
+				console.log('Box dialog has been closed');
+				return;
+			}
+
+			navigator.serviceWorker.getRegistration().then(reg => {
+				reg.showNotification('Notification granted');
+			});
+		});
+	}
+};
+
+// Register service worker and push notification
 if ('serviceWorker' in navigator) {
 	const registration = runtime.register();
+	requestPermission();
 } else {
 	console.log('ServiceWorker belum didukung browser ini.');
 }
--- src/sw.js
+++ src/sw.js
@@ -105,3 +105,26 @@ self.addEventListener('fetch', event => {
 		);
 	}
 });
+
+// Push notification
+self.addEventListener('push', event => {
+	let body;
+
+	if (event.data) {
+		body = event.data.text();
+	} else {
+		body = 'Push message no payload';
+	}
+
+	const options = {
+		body: body,
+		icon: './assets/icons/favicon.png',
+		vibrate: [100, 50, 100],
+		data: {
+			dateOfArrival: Date.now(),
+			primaryKey: 1,
+		},
+	};
+
+	event.waitUntil(self.registration.showNotification('Push Notification', options));
+});
