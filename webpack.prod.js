aba1d61b325b090ef991c3913ee2482157175cb3
muhazizal <muhazizal.26@gmail.com>
2020-07-11 18:05:22 +0700

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -35,26 +35,6 @@ module.exports = merge(common, {
 		],
 	},
 	optimization: {
-		splitChunks: {
-			chunks: 'async',
-			minSize: 30000,
-			maxSize: 0,
-			minChunks: 1,
-			maxAsyncRequests: 6,
-			maxInitialRequests: 4,
-			automaticNameDelimiter: '~',
-			cacheGroups: {
-				defaultVendors: {
-					test: /[\\/]node_modules[\\/]/,
-					priority: -10,
-				},
-				default: {
-					minChunks: 2,
-					priority: -20,
-					reuseExistingChunk: true,
-				},
-			},
-		},
 		minimize: true,
 		minimizer: [
 			// Minify js plugin
